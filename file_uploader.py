eb49890d47370caa209c084857e68535ac98c64e
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-09-09 14:57:42 +0100

    added codes to be able to upload pic to the app

--- file_uploader.py
+++ file_uploader.py
@@ -1,25 +1,31 @@
 import tkinter as tk
-from tkinter import Label
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+screen_width = 1200
+screen_height = 790
 
 def imageUploader():
     fileTypes = [("Image Files", "*.jpg *.jpeg *.png")]
     path = tk.filedialog.askopenfilename(filetypes=fileTypes)
 
+
     # if file is selected
     if len(path):
-        for file in path:
-            img = Image.open(path)
-            uploaded_img = img.resize('600x350')
-            pic = ImageTk.PhotoImage(uploaded_img)
+        img = Image.open(path)
+        img_w, img_h = img.size
+        if img_w <= screen_width and img_h <= screen_height:
+            pic = ImageTk.PhotoImage(img)
 
-            app.geometry('600x350')
-            # clear canvas and redraw
-            canvas.delete("all")
-            canvas.create_image(0, 0, image=pic, anchor="nw")
-            canvas.image = pic  # keep a reference!
+        # Otherwise, resize proportionally to fit in screen
+        img.thumbnail((screen_width, screen_height), Image.LANCZOS)
+        pic = ImageTk.PhotoImage(img)
+
+        app.geometry('1200x790')
+        # clear canvas and redraw
+        canvas.delete("all")
+        canvas.create_image(600, 395, image=pic, anchor="center")
+        canvas.image = pic  # keep a reference!
 
     # if no file is selected
     else:
@@ -30,22 +36,22 @@ if __name__ == '__main__':
     app = tk.Tk()
     # setting title and basic size to the app
     app.title("Image Watermarking Desktop App")
-    app.geometry('600x350')
+    app.geometry('1200x790')
 
     # add background image
     bg_img = Image.open("background.png")         # open image with PIL
-    bg_img = bg_img.resize((600, 350))
+    bg_img = bg_img.resize((screen_width, screen_height))
     img = ImageTk.PhotoImage(bg_img)
 
-    canvas = tk.Canvas(app, width=600, height=350)
+    canvas = tk.Canvas(app, width=screen_width, height=screen_height)
     canvas.pack()
 
     canvas.create_image(0, 0, image=img, anchor='nw')
     canvas.image = img
 
     # overlay text directly on canvas
-    canvas.create_text(300, 50, text='Image Watermaking App',
-                       font=('Arial', 25, 'bold'), fill='darkgrey')
+    canvas.create_text(600, 100, text='Image Watermaking App',
+                       font=('Arial', 25, 'bold'), fill='black')
 
     # define upload button
     uploadButton = tk.Button(app, text='Upload Image',
