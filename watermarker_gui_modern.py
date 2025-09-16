bf4be22daa03fe2af468f48da5b12c624e02221e
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-09-16 15:01:05 +0100

    created another repo for modern ver and moved all the related files there

--- watermarker_gui_modern.py
+++
@@ -1,7 +0,0 @@
-import customtkinter as ctk
-
-# set the default color theme for the whole app
-ctk.set_default_color_theme('autumn.json')
-# appearance available system/light/dark
-ctk.set_appearance_mode('system')
-
